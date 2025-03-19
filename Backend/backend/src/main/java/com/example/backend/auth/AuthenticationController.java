9e607bd53f2d162f17b3d0dc4d101708cdcb2b55
sihle1996 <mofokengsihle63@gmail.com>
2025-03-19 03:18:30 -0700

    added authenticationcontroller

--- Backend/backend/src/main/java/com/example/backend/auth/AuthenticationController.java
+++ Backend/backend/src/main/java/com/example/backend/auth/AuthenticationController.java
@@ -1,4 +1,40 @@
 package com.example.backend.auth;
 
+
+import com.example.backend.user.User;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@CrossOrigin(origins = "*")
+@RestController
+@RequestMapping("/api")
+@RequiredArgsConstructor
 public class AuthenticationController {
+
+    private final AuthenticationService service;
+
+    @PostMapping("/register")
+    public ResponseEntity<AuthenticationResponse> register(@RequestBody RegisterRequest request) {
+        return ResponseEntity.ok(service.register(request));
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<AuthenticationResponse> authenticate(@RequestBody AuthenticationRequest request) {
+        return ResponseEntity.ok(service.authenticate(request));
+    }
+
+    @GetMapping("/users")
+    public ResponseEntity<List<User>> getAllUsers() {
+        List<User> users = service.getAllUsers();
+        return ResponseEntity.ok(users);
+    }
+
+    @GetMapping("/users/{id}")
+    public ResponseEntity<User> getUserById(@PathVariable Long id) {
+        User user = service.findById(id);
+        return ResponseEntity.ok(user);
+    }
 }
