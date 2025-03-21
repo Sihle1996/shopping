8cbca6540742de0e3d44df6735c016a5c95ad6ea
sihle1996 <mofokengsihle63@gmail.com>
2025-03-20 23:52:16 -0700

    added orders page

--- Frontend/src/app/pages/historyorders/historyorders.component.ts
+++ Frontend/src/app/pages/historyorders/historyorders.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Order, OrderService } from 'src/app/services/order.service';
 
 @Component({
   selector: 'app-historyorders',
@@ -6,5 +7,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./historyorders.component.scss']
 })
 export class HistoryordersComponent {
+  orders: Order[] = [];
+  loading = true;
+  errorMessage = '';
 
+  constructor(private orderService: OrderService) {}
+
+  ngOnInit(): void {
+    this.orderService.getUserOrders().subscribe({
+      next: (data) => {
+        this.orders = data;
+        this.loading = false;
+      },
+      error: (err) => {
+        this.errorMessage = 'Failed to fetch orders.';
+        console.error("❌ Error loading orders:", err);
+        this.loading = false;
+      }
+    });
+  }
 }
